9bdcf9c63856b03dfd0809e2921a554ff6849c31
Saman833 <s.ahmadifar2000@gmail.com>
2025-03-10 11:39:33 +0200

    check practices in the class

--- src/main/java/saman/demo/homework1/DataInitializer.java
+++ src/main/java/saman/demo/homework1/DataInitializer.java
@@ -10,12 +10,10 @@ public class DataInitializer {
     public DataInitializer(BookCatalog bookCatalog, ReviewManager reviewManager, Book book1, Book book2, Book book3) {
         System.out.println("Constructor Injection: DataInitializer Created");
 
-        // Add books to catalog
         bookCatalog.addBook(book1);
         bookCatalog.addBook(book2);
         bookCatalog.addBook(book3);
 
-        // Add reviews for each book
         reviewManager.addReview(new Review(book1, "5", "Amazing dystopian novel!"));
         reviewManager.addReview(new Review(book2, "4", "A powerful story of injustice."));
         reviewManager.addReview(new Review(book3, "3", "A timeless classic."));
